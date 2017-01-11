3dcb29dbfae57687d01e721bfc32e89da25e761d
Bob Freeman <rfreeman@hbs.edu>
2017-01-11 12:10:08 -0500

    example scripts

---
+++ multiple_approaches_combining_data/scripts/bad_query.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+def get_name(database_file, person_id):
+    query = "SELECT personal || ' ' || family FROM Person WHERE id='" + person_id + "';"
+
+    connection = sqlite3.connect(database_file)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    return results[0][0]
+
+print "full name for dyer:", get_name('survey.db', 'dyer')
---
+++ multiple_approaches_combining_data/scripts/db_query.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+connection = sqlite3.connect("survey.db")
+cursor = connection.cursor()
+cursor.execute("SELECT Site.lat, Site.long FROM Site;")
+results = cursor.fetchall()
+for r in results:
+    print r
+cursor.close()
+connection.close()
